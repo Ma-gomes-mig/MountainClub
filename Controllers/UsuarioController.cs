a7755950e49d6e317d9450659ef582434dfe98b4
Matheus Gomes <matheus.gom23@yahoo.com>
2022-12-18 21:11:18 -0300

    Criação do UsuarioController e da view Usuario

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoSite1.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
